db16b6412977dfd5aefe0adfba0e24f79b734bc5
Matthew Gusmano <matthew.gusmano@gmail.com>
2016-04-27 22:34:09 -0400

    added gem clicks

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,9 +5,13 @@ $(document).ready(function () {
 
 	
 
-		wins: 0;
-		loses: 0;
-		userstart: 0;
+		var wins = 0;
+		var loses= 0;
+		var userStart = 0;
+		randomCrystalScore1 = (Math.floor(Math.random() * 12));
+		randomCrystalScore2 = (Math.floor(Math.random() * 12));
+		randomCrystalScore3 = (Math.floor(Math.random() * 12));
+		randomCrystalScore4 = (Math.floor(Math.random() * 12));
 		//This is the selection of 4 crystals
 		// CrystalsMain : [
 		// 	crystal1 = {
@@ -27,13 +31,38 @@ $(document).ready(function () {
 		function compNumber(num) {
 			return Math.floor(Math.random() * 26 +num)
 		};
-			var randComp = compNumber(25);
-			$('#score').html('<h3>' + randComp+ '</h3>');
-			console.log(randComp);
+		var randComp = compNumber(25);
+		$('#score').html('<h3>' + randComp+ '</h3>');
+		console.log(randComp);
+
+
+		$('#userscore').html('<h3>' +userStart+ '</h3>');
+
+
+
+
+		
 					
+		//clicking event for each gem initiated
+
+		$(".gem1").on("click", function () {
+			userStart += randomCrystalScore1;
+			$('#userscore').html('<h3>' +userStart+ '</h3>')
+		});
+		$(".gem2").on("click", function () {
+			userStart += randomCrystalScore2;
+			$('#userscore').html('<h3>' +userStart+ '</h3>')
+		});
+		$(".gem3").on("click", function () {
+			userStart += randomCrystalScore3;
+			$('#userscore').html('<h3>' +userStart+ '</h3>')
+		});
+		$(".gem4").on("click", function () {
+			userStart += randomCrystalScore4;
+			$('#userscore').html('<h3>' +userStart+ '</h3>')
+		});
 		
 
-		// $("#gem1").on("click", function () {
 
 		// }),
 		
